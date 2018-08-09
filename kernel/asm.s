216dbcb8805ff0c12875a62cae460e0307ae28b1
Travor Liu <travor_lzh@outlook.com>
2018-08-10 00:21:32 +0800

    kernel: Assembly part of IRQ

--- kernel/asm.s
+++ kernel/asm.s
@@ -1,5 +1,6 @@
 .code32
 .extern	int_handler
+.extern	irq_handler
 DATASEG=0x10
 
 .macro	INT_NOERRCODE	no
@@ -52,7 +53,6 @@ INT_NOERRCODE	29
 INT_NOERRCODE	30
 INT_NOERRCODE	31
 
-.globl	int_stub
 int_stub:
 	pusha
 	movw	%ds,%ax
@@ -71,6 +71,54 @@ int_stub:
 	movw	%ax,%es
 	movw	%ax,%fs
 	movw	%ax,%gs
+	popa
+	addl	$8,%esp
+	iret
+
+.macro	IRQ	no,int_no
+	.globl	irq\no
+	irq\no:
+		push	$0
+		push	$\int_no
+		jmp	irq_stub
+.endm
+
+IRQ	0 32
+IRQ	1 33
+IRQ	2 34
+IRQ	3 35
+IRQ	4 36
+IRQ	5 37
+IRQ	6 38
+IRQ	7 39
+IRQ	8 40
+IRQ	9 41
+IRQ	10 42
+IRQ	11 43
+IRQ	12 44
+IRQ	13 45
+IRQ	14 46
+IRQ	15 47
+
+irq_stub:
+	pusha
+	movw	%ax,%ds
+	pushl	%eax
+
+	movw	$DATASEG,%ax
+	movw	%ax,%ds
+	movw	%ax,%es
+	movw	%ax,%fs
+	movw	%ax,%gs
+
+	call	irq_handler
+
+	popl	%ebx
+	movw	%bx,%ds
+	movw	%bx,%es
+	movw	%bx,%fs
+	movw	%bx,%gs
+
 	popa
 	addl	$8,%esp
 	iret
