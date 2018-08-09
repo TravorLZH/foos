1e6889263a0cd5ca984c67bf5860bcba9f0b1142
Travor Liu <travor_lzh@outlook.com>
2018-08-09 13:45:12 +0800

    kernel: Write string using TTY device

--- kernel/main.c
+++ kernel/main.c
@@ -1,8 +1,12 @@
 /* Entry of the kernel */
+#include <system.h>
+#include <dev/tty.h>
+
+struct tty kernel_tty;
 
 int kernel_main(void)
 {
-	short *ptr=(short*)0xB8000;
-	*ptr=0x0F42;
+	tty_init(&kernel_tty);
+	tty_writestring(&kernel_tty,"Hello world\n");
 	return 0;
 }
