a4e2b6d23c3a97ab419ace067af7a72c2f951b4f
Travor Liu <travor_lzh@outlook.com>
2018-08-10 00:21:12 +0800

    header: Declare IRQ stuff

--- include/cpu/interrupt.h
+++ include/cpu/interrupt.h
@@ -60,4 +60,21 @@ extern void int28(void);
 extern void int29(void);
 extern void int30(void);
 extern void int31(void);
+
+extern void irq0(void);
+extern void irq1(void);
+extern void irq2(void);
+extern void irq3(void);
+extern void irq4(void);
+extern void irq5(void);
+extern void irq6(void);
+extern void irq7(void);
+extern void irq8(void);
+extern void irq9(void);
+extern void irq10(void);
+extern void irq11(void);
+extern void irq12(void);
+extern void irq13(void);
+extern void irq14(void);
+extern void irq15(void);
 #endif
