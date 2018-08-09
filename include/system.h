3aa551830c25c658fcfc56448015de0bd5b7ad7e
Travor Liu <travor_lzh@outlook.com>
2018-08-09 16:35:53 +0800

    header: Make kernel_tty visible to the libraries

--- include/system.h
+++ include/system.h
@@ -3,6 +3,9 @@
 #define	CODESEG	0x08
 #define	DATASEG	0x08
 #define	NULL	(void*)0
+#include <dev/tty.h>
+/* Common Variables */
+extern struct tty kernel_tty;
 /* Functions */
 extern int kernel_main(void);
 #endif
