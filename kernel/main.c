a37dbb6746781022f8489fd2cfa9b16d93ee1437
Travor Liu <travor_lzh@outlook.com>
2018-08-09 22:33:51 +0800

    kernel: Test zero division interrupt

--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,7 @@
 /* Entry of the kernel */
 #include <system.h>
 #include <dev/tty.h>
+#include <cpu/interrupt.h>
 #include <stdio.h>
 
 struct tty kernel_tty;
@@ -8,6 +9,7 @@ struct tty kernel_tty;
 int kernel_main(void)
 {
 	tty_init(&kernel_tty);
-	puts("Hello world");
+	idt_init();
+	int a=3/0;
 	return 0;
 }
