f6a3c5067dcdbec16d6612bb703e644231fe64bb
Travor Liu <travor_lzh@outlook.com>
2018-08-09 16:35:12 +0800

    kernel: Use library functions

--- kernel/main.c
+++ kernel/main.c
@@ -1,12 +1,13 @@
 /* Entry of the kernel */
 #include <system.h>
 #include <dev/tty.h>
+#include <stdio.h>
 
 struct tty kernel_tty;
 
 int kernel_main(void)
 {
 	tty_init(&kernel_tty);
-	tty_writestring(&kernel_tty,"Hello world\n");
+	puts("Hello world");
 	return 0;
 }
