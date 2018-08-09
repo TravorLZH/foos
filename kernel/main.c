5baacf1095ba04290ab8971aee20e07c12b5e098
Travor Liu <travor_lzh@outlook.com>
2018-08-10 00:22:20 +0800

    kernel: Testing Timer IRQ

--- kernel/main.c
+++ kernel/main.c
@@ -6,11 +6,17 @@
 
 struct tty kernel_tty;
 
+void timer_irq(struct registers regs)
+{
+	puts("Timer IRQ");
+}
+
 int kernel_main(void)
 {
 	tty_init(&kernel_tty);
 	int_init();
 	__asm__("int $0x1");
-	int a=3/0;
+	int_hook_handler(0x20,timer_irq);
+	__asm__("sti");
 	return 0;
 }
