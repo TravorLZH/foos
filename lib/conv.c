be6abdbd60ce758e9f1da4a583648c05693bffce
Travor Liu <travor_lzh@outlook.com>
2018-08-11 13:33:22 +0800

    lib: Conversions for unsigned ints, hex

--- lib/conv.c
+++ lib/conv.c
@@ -40,3 +40,41 @@ void itoa(int n,char *s)
 	*x='\0';
 	strrev(s);
 }
+
+void utoa(uint32_t n,char *s)
+{
+	char *x=s;
+	do{
+		*x=n%10+'0';
+		x++;
+	}while(n/=10);
+	*x='\0';
+	strrev(s);
+}
+
+/* Hex conversions */
+size_t num_hex_digits(size_t n)
+{
+	int ret=0;
+	if(n==0)return 1;
+	while(n!=0){
+		n >>= 4;
+		ret++;
+	}
+	return ret;
+}
+
+void itoh(uint32_t n,char* s)
+{
+	int len=num_hex_digits(n);
+	const char hex_lookup[]="0123456789ABCDEF";
+
+	if(len & 1){
+		*s++ = '0';
+	}
+	s[len]='\0';
+	for(--len; len>=0; n>>= 4, --len){
+		s[len]=hex_lookup[n & 0xF];
+	}
+}
+
