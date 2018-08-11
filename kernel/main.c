7e4adc94c82c9de15d55826bce26d2817548dbf1
Travor Liu <travor_lzh@outlook.com>
2018-08-11 13:37:53 +0800

    kernel: Testing keyboard IRQ with printf

--- kernel/main.c
+++ kernel/main.c
@@ -1,22 +1,24 @@
 /* Entry of the kernel */
 #include <system.h>
 #include <dev/tty.h>
+#include <asm/ioports.h>
 #include <cpu/interrupt.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 struct tty kernel_tty;
 
-void timer_irq(struct registers regs)
+void kbd_irq(struct registers regs)
 {
-	puts("Timer IRQ");
+	puts("Keyboard");
+	printf("Scancode: 0x%x\n",(uint32_t)inb(0x60));
 }
 
 int kernel_main(void)
 {
 	tty_init(&kernel_tty);
 	int_init();
-	__asm__("int $0x1");
-	int_hook_handler(0x20,timer_irq);
+	int_hook_handler(0x21,kbd_irq);
 	__asm__("sti");
 	return 0;
 }
