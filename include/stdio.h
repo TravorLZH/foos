55bc3ef629637c2adf964918c265d6345e52dbc6
Travor Liu <travor_lzh@outlook.com>
2018-08-11 13:35:01 +0800

    header: Add printf family

--- include/stdio.h
+++ include/stdio.h
@@ -1,5 +1,15 @@
 #ifndef	STDIO_H
 #define	STDIO_H
+#include <stdarg.h>
+#include <inttypes.h>
+#ifndef	BUFSIZ
+#define	BUFSIZ	512
+#endif
 extern int puts(const char *str);
 extern int putchar(int c);
+
+extern int printf(const char *fmt,...);
+extern int sprintf(char *str,const char *fmt,...);
+extern int vprintf(const char *fmt,va_list ap);
+extern int vsprintf(char *str,const char *fmt,va_list ap);
 #endif
