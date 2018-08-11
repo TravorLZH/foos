2eb137c51db2546a208d6f6edc37d7fea5cd0fda
Travor Liu <travor_lzh@outlook.com>
2018-08-11 15:36:46 +0800

    kernel: entry: Enable A20 before kernel_main()

--- kernel/entry.s
+++ kernel/entry.s
@@ -9,6 +9,23 @@ kernel_entry:
 	movw	%ax,%fs
 	movw	%ax,%gs
 	movw	%ax,%ss
-	movl	$0x10000,%esp
+check_a20:
+	pusha
+	movl	$0x100000,%edi
+	movl	$0x200000,%esi
+	movl	%esi,(%esi)
+	movl	%edi,(%edi)
+	cmpsd	# See if they are equal
+	popa
+	jne	a20_ok
+enable_a20_fast:
+	inb	$0x92,%al
+	testb	$0x2,%al
+	jne	a20_ok
+	orb	$0x2,%al
+	andb	$0xFE,%al
+	outb	%al,$0x92
+a20_ok:
+	movl	$0x200000,%esp	# 1MB Stack (0x100000 - 0x200000)
 	call	kernel_main
 	jmp	.
