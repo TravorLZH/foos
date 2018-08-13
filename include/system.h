3607ae01df92e8da9c3fe85b72e4042ffda7c16a
Travor Liu <travor_lzh@outlook.com>
2018-08-13 16:25:33 +0800

    header: system.h: Define BUFSIZ

--- include/system.h
+++ include/system.h
@@ -1,10 +1,13 @@
 #ifndef	SYSTEM_H
 #define	SYSTEM_H
+#include <dev/tty.h>
 #define	CODESEG	0x08
 #define	DATASEG	0x08
 #define	NULL	(void*)0
 #define	hang()	while(1)
-#include <dev/tty.h>
+#ifndef	BUFSIZ
+#define	BUFSIZ	512
+#endif
 /* Common Variables */
 extern struct tty kernel_tty;
 /* Functions */
