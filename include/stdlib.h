71693977ffa8d753288139e2bb129713c4013d38
Travor Liu <travor_lzh@outlook.com>
2018-08-13 16:24:53 +0800

    header: stdlib.h: Define NULL and BUFSIZ

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,5 +1,11 @@
 #ifndef	STDLIB_H
 #define	STDLIB_H
+#ifndef	BUFSIZ
+#define	BUFSIZ	512
+#endif
+#ifndef	NULL
+#define	NULL	(void*)0
+#endif
 extern int atoi(const char *nptr);
 extern void itoa(int n,char *s);
 #endif
