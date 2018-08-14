1ebc320b1e40886c5091d1ceef18cdbc53d537ba
Travor Liu <travor_lzh@outlook.com>
2018-08-15 00:39:49 +0800

    header: Use placement malloc to allocate data structures

---
+++ include/foos/kmalloc.h
@@ -0,0 +1,10 @@
+#ifndef	KMALLOC_H
+#define	KMALLOC_H
+#include <inttypes.h>
+#define	HEAP_BASE	0x400000
+#define	HEAP_SIZE	0x400000
+
+#define	PAGE_ALIGN	12	// 4 KB Aligned
+extern void *kmalloc(size_t sz);
+extern void *kmalloca(size_t sz,size_t align);
+#endif
