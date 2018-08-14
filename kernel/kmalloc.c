b5c3f3bef65131a3b4eefc78d337d26d4d824974
Travor Liu <travor_lzh@outlook.com>
2018-08-15 00:40:11 +0800

    kernel: Implement placement malloc

---
+++ kernel/kmalloc.c
@@ -0,0 +1,26 @@
+#include <foos/kmalloc.h>
+#include <foos/system.h>
+#include <inttypes.h>
+
+char *heap=(char *)HEAP_BASE;
+
+void *kmalloc(size_t sz)
+{
+	void *ret=heap;
+	heap+=sz;
+	return ret;
+}
+
+void *kmalloca(size_t sz,size_t align)
+{
+	size_t tmp=(size_t)heap;
+	size_t x=0xFFFFFFFF << align;
+	align=1 << align;
+	if(tmp%align)
+	{
+		tmp&=x;
+		tmp+=align;
+		heap=(char*)tmp;
+	}
+	return kmalloc(sz);
+}
