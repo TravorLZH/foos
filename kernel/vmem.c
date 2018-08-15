34c276b0233cd722fbd88757e029890b7dbc76f0
Travor Liu <travor_lzh@outlook.com>
2018-08-15 12:27:38 +0800

    kernel: No need to allocate page tables myself

--- kernel/vmem.c
+++ kernel/vmem.c
@@ -8,8 +8,6 @@
 #define	TABLE_SIZE	1024*sizeof(struct page)
 #define	DIR_SIZE	1024*sizeof(struct page_table)
 
-struct page *kernel_table=NULL;
-struct page *kernel_heap=NULL;
 struct page_table *tables=NULL;
 
 extern void vmem_enable(struct page_table *dir);
@@ -32,40 +30,38 @@ static void page_fault(struct registers regs)
 
 int vmem_init(void *reserved)
 {
-	if(kernel_table==NULL){
-		kernel_table=kmalloca(TABLE_SIZE,PAGE_ALIGN);
-		memset(kernel_table,0,TABLE_SIZE);
-	}
-	if(kernel_heap==NULL){
-		kernel_heap=kmalloca(TABLE_SIZE,PAGE_ALIGN);
-		memset(kernel_heap,0,TABLE_SIZE);
-	}
 	if(tables==NULL){
 		tables=kmalloca(DIR_SIZE,PAGE_ALIGN);
 		memset(tables,0,DIR_SIZE);
 	}
-	size_t i;
+	size_t i,j;
 	for(i=0;i<1024;i++){
-		kernel_table[i].present=1;
-		kernel_table[i].writable=1;
-		kernel_table[i].user=0;
-		kernel_table[i].frame=i;
+		for(j=0;j<1024;j++){
+			void *addr=(void*)((i<<22)+(j<<PAGE_ALIGN));
+			pmem_mapaddr(addr,addr,tables,P_WRITABLE);
+		}
 	}
-	for(i=0;i<1024;i++){
-		kernel_heap[i].present=1;
-		kernel_heap[i].writable=1;
-		kernel_heap[i].user=0;
-		kernel_heap[i].frame=i+1024;
-	}
-	tables[0].present=1;
-	tables[0].writable=1;
-	tables[0].user=0;
-	tables[0].page=(size_t)kernel_table >> PAGE_ALIGN;
-	tables[1].present=1;
-	tables[1].writable=1;
-	tables[1].user=0;
-	tables[1].page=(size_t)kernel_heap >> PAGE_ALIGN;
 	int_hook_handler(0x0E,page_fault);
 	vmem_enable(tables);
 	return 0;
+}
+
+struct page *vmem_get(void *addr,struct page_table *dir)
+{
+	if(dir==NULL){
+		__asm__("movl %%cr3,%%eax":"=a"(dir));
+	}
+	size_t tmp=(size_t)addr/PAGE_SIZE;
+	size_t table_i=tmp/1024;
+	size_t offset=tmp%1024;
+	struct page *table=(struct page*)(dir[table_i].page << PAGE_ALIGN);
+	if(!dir[table_i].present || table==NULL){
+		table=(struct page*)
+			kmalloca(TABLE_SIZE,PAGE_ALIGN);
+		dir[table_i].present=1;
+		dir[table_i].writable=0;
+		dir[table_i].user=0;
+		dir[table_i].page=(size_t)table >> PAGE_ALIGN;
+	}
+	return table+offset;
 }
