2a2d55a78a78f255a57fd4642af9c51ccb023930
Travor Liu <travor_lzh@outlook.com>
2018-10-02 23:22:50 +0800

    ARM Hello world
    * .gitignore: Ignore kernel.elf
    * arm/start.S: Do nothing but jump to entry()
    * arm/main.c: Send "Hello world!\n"

--- arm/main.c
+++ arm/main.c
@@ -1,4 +1,18 @@
+void serial_sendbyte(char c)
+{
+	__asm__("ldr r0,=#0x101f1000\n"
+		"str %[val],[r0]"::[val]"r"((long)c));
+}
+
+int serial_sendstring(const char *s)
+{
+	do{
+		serial_sendbyte(*s);
+	}while(*++s);
+}
+
 int entry(void)
 {
+	serial_sendstring("Hello world!\n");
 	return 0;
 }
