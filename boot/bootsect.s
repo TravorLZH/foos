450ef2bca946fb9592671e355bce30eff762d7ba
Travor Liu <travor_lzh@outlook.com>
2019-03-23 21:18:15 +0800

    Use extended BIOS read to load `system'
    I have been dealing with this DAP table for many weeks, and now I just
    realized that the last argument `LBA' starts from 0 instead of 1.
    This commit marks the advancement of FOOS development because using
    BIOS's legacy read is inconvenient to load larger kernels.
    
    What a happy day!
    
    * boot/bootsect.s: Call int 0x13, ah=0x42 to load system instead of
    	int 0x13, ah=0x02

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -55,31 +55,15 @@ load_setup:
 	movw	$0x0002,%cx	# From cylinder 0, 2nd sector
 	int	$0x13
 	jc	disk_error
-#	jmp	load_system_new
-# FIXME: The system is larger, the method of reading it oughts to be improved
-load_system:
-	movw	$loading_system,%si
-	call	print_string
-	stc
-	movw	$SYSSEG,%ax
-	movw	%ax,%es
-	movw	$0x0,%bx
-	movb	$0x2,%ah
-	movw	$0x0003,%cx
-	movb	$SYS_SECTORS,%al
-	int	$0x13
-	jc	disk_error
-	cmpb	$SYS_SECTORS,%al	# Make sure BIOS read enough for us
-	jne	disk_error
-	jmp	run_setup
-# This loads system using DAP table, but not working
-load_system_new:
+# This loads system using DAP table, WORKING!!!
+load_system_ext:
 	movw	$kernel_dap,%si
 	movb	$0x42,%ah
 	int	$0x13
 	jc	disk_error
 	cmp	$0x0,%ah
 	jne	disk_error
+
 run_setup:
 	ljmp	$SETUPSEG,$0x0	# Let's go
 
@@ -110,7 +94,7 @@ kernel_dap:
 .byte	0
 .word	SYS_SECTORS	# no. of sectors
 .word	0x0,SYSSEG	# Transfer buffer
-.long	0x03,0x0	# Starting LBA
+.long	0x02,0x0	# Starting LBA
 
 # Strings
 should_boot_from_hda:
